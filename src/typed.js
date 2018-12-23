164c5961ba012ce8055a13763a794fc693e7da88
TK <leandrotk100@gmail.com>
2018-12-23 14:10:35 -0200

    Add typed about me info and writing test

---
+++ src/typed.js
@@ -0,0 +1,15 @@
+let aboutStrings = [
+  "software engineer",
+  "lifelong learner",
+  "problem solver",
+  "blogger"
+];
+
+let options = {
+  strings: aboutStrings,
+  typeSpeed: 50,
+  loop: true,
+  backDelay: 1200
+};
+
+new Typed(".js-about-info", options);
