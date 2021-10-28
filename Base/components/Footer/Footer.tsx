a8af907abb67c9d68935e4414c0677fea8a3ab05
TK <leandrotk100@gmail.com>
2021-10-28 15:25:40 -0300

    footer

---
+++ Base/components/Footer/Footer.tsx
@@ -0,0 +1,36 @@
+import type { NextPage } from 'next';
+
+export const Footer: NextPage = () => (
+  <footer id="footer">
+    <div className="footer-left">Copyright &copy; 2021 TK</div>
+    <div className="footer-right">
+      <nav>
+        <ul>
+          <li>
+            <a href="index.html">Home</a>
+          </li>
+          <li>
+            <a href="writing.html">Writing</a>
+          </li>
+          <li>
+            <a
+              href="https://github.com/leandrotk"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Projects
+            </a>
+          </li>
+          <li>
+            <a href="quotes.html">Quotes</a>
+          </li>
+          <li>
+            <a href="rss.xml" target="_blank" rel="noopener noreferrer">
+              RSS
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  </footer>
+);
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,7 @@ import { About } from '../Home/components/About';
 import { Writings } from '../Home/components/Writings';
 import { Series } from '../Home/components/Series';
 import { Projects } from '../Home/components/Projects';
+import { Footer } from '../Base/components/Footer';
 
 const Home: NextPage = () => {
   return (
@@ -20,39 +21,7 @@ const Home: NextPage = () => {
           <Projects />
         </div>
       </main>
-
-      <footer id="footer">
-        <div className="footer-left">Copyright &copy; 2021 TK</div>
-        <div className="footer-right">
-          <nav>
-            <ul>
-              <li>
-                <a href="index.html">Home</a>
-              </li>
-              <li>
-                <a href="writing.html">Writing</a>
-              </li>
-              <li>
-                <a
-                  href="https://github.com/leandrotk"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  Projects
-                </a>
-              </li>
-              <li>
-                <a href="quotes.html">Quotes</a>
-              </li>
-              <li>
-                <a href="rss.xml" target="_blank" rel="noopener noreferrer">
-                  RSS
-                </a>
-              </li>
-            </ul>
-          </nav>
-        </div>
-      </footer>
+      <Footer />
     </>
   );
 };
