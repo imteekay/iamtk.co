8640bf09c5fc1850f946847eb449ea1604a866cc
TK <leandrotk100@gmail.com>
2021-12-07 23:37:47 -0300

    ga

--- Base/Analytics/GAScript/GAScript.tsx
+++ Base/Analytics/GAScript/GAScript.tsx
@@ -1,21 +1,24 @@
+import Script from 'next/script';
 import { GA_TRACKING_ID } from 'src/lib/tracking/gtag';
 
 export const GAScript = () => (
   <>
-    <script
-      async
+    <Script
+      strategy="afterInteractive"
       src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
     />
-    <script
+    <Script
+      id="gtag-init"
+      strategy="afterInteractive"
       dangerouslySetInnerHTML={{
         __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${GA_TRACKING_ID}', {
-              page_path: window.location.pathname,
-            });
-          `,
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${GA_TRACKING_ID}', {
+            page_path: window.location.pathname,
+          });
+        `,
       }}
     />
   </>
