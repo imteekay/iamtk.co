1f8d2f33125547e89d53473d25d2da91dada147b
TK <leandrotk100@gmail.com>
2021-12-08 13:14:44 -0300

    GA attempt

--- Base/Analytics/GAScript/GAScript.tsx
+++ Base/Analytics/GAScript/GAScript.tsx
@@ -1,15 +1,12 @@
-import Script from 'next/script';
 import { GA_TRACKING_ID } from 'src/lib/tracking/gtag';
 
 export const GAScript = () => (
   <>
-    <Script
-      strategy="afterInteractive"
+    <script
+      async
       src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
     />
-    <Script
-      id="gtag-init"
-      strategy="afterInteractive"
+    <script
       dangerouslySetInnerHTML={{
         __html: `
           window.dataLayer = window.dataLayer || [];
