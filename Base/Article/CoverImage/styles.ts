cf336bb58debf7d4c64a2f6aab37daba194e884f
TK <leandrotk100@gmail.com>
2021-12-19 11:26:43 -0300

    fix link on hover

--- Base/Article/CoverImage/styles.ts
+++ Base/Article/CoverImage/styles.ts
@@ -10,10 +10,4 @@ export const figcaptionStyle = {
 
 export const figcaptionLinkStyle = {
   textDecoration: 'none',
-  color: 'white',
-  backgroundRepeat: 'repeat-x',
-  backgroundPosition: 'bottom',
-  backgroundSize: '100% 6px',
-  backgroundImage:
-    'linear-gradient( transparent, transparent 5px, white 5px, white )',
 };
