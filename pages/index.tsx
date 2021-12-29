93e2ee8274d17669a1b4a67b928547359113ee62
TK <leandrotk100@gmail.com>
2021-12-29 19:21:36 -0300

    testing minisearch

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,9 +7,30 @@ import { Writings } from 'Home/components/Writings';
 import { Series } from 'Home/components/Series';
 import { Projects } from 'Home/components/Projects';
 
+import MiniSearch from 'minisearch';
+import { useState } from 'react';
+import { postsList } from 'Home/components/Writings/postsList';
+
+let miniSearch = new MiniSearch({
+  fields: ['title', 'tags'],
+  storeFields: ['title', 'tags'],
+});
+
+miniSearch.addAll(postsList);
+
 const Page: NextPage = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const a = miniSearch.search(searchTerm);
+  console.log('a', a);
   return (
     <>
+      <input
+        onChange={(event) => {
+          console.log('event.target.value', event.target.value),
+            setSearchTerm(event.target.value);
+        }}
+      />
       <Head
         title="TK"
         description="Learning & Improving with TK"
