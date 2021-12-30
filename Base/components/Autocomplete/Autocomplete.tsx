a68b3437ec51dc1eb13435649249b3c402b6cfee
TK <leandrotk100@gmail.com>
2021-12-30 17:14:05 -0300

    style autocomplete

--- Base/components/Autocomplete/Autocomplete.tsx
+++ Base/components/Autocomplete/Autocomplete.tsx
@@ -1,6 +1,8 @@
 import { FC, useState, Dispatch, SetStateAction } from 'react';
+import { css } from '@emotion/css';
 import MiniSearch, { Suggestion } from 'minisearch';
 import ReactAutocomplete from 'react-autocomplete';
+import { Icon } from './Icon';
 
 type Options = {
   fields: string[];
@@ -34,19 +36,68 @@ export const Autocomplete: FC<AutocompletePropTypes> = ({
   suggestions,
   updateSearchTerm,
 }) => (
-  <ReactAutocomplete
-    getItemValue={(item) => item.suggestion}
-    items={suggestions}
-    renderItem={(item, isHighlighted) => (
-      <div
-        key={item.title}
-        style={{ background: isHighlighted ? 'black' : 'lightgray' }}
-      >
-        {item.suggestion}
-      </div>
-    )}
-    value={searchTerm}
-    onChange={(e) => updateSearchTerm(e.target.value)}
-    onSelect={(value) => updateSearchTerm(value)}
-  />
+  <div
+    className={css`
+      display: flex;
+      align-items: center;
+      gap: 6px;
+      border-radius: 4px;
+      padding: 8px;
+      border: 0.1px solid white;
+      background-color: black;
+      color: white;
+      font-family: 'Menlo', monospace;
+      width: 232px;
+
+      input {
+        border: none;
+        background-color: black;
+        color: white;
+        font-family: 'Menlo', monospace;
+        font-size: 15px;
+
+        &:focus,
+        &:focus-visible {
+          outline: none;
+        }
+      }
+    `}
+  >
+    <Icon src="/search.svg" />
+    <ReactAutocomplete
+      getItemValue={(item) => item.suggestion}
+      items={suggestions}
+      renderItem={(item, isHighlighted) => (
+        <div
+          key={item.suggestion}
+          style={{
+            background: isHighlighted
+              ? 'rgba(255, 255, 255, 0.1)'
+              : 'rgb(27, 27, 27)',
+            padding: '6px 8px',
+            display: 'flex',
+          }}
+        >
+          {item.suggestion}
+        </div>
+      )}
+      value={searchTerm}
+      onChange={(e) => updateSearchTerm(e.target.value)}
+      onSelect={(value) => updateSearchTerm(value)}
+      wrapperStyle={{
+        position: 'relative',
+        display: 'flex',
+      }}
+      menuStyle={{
+        position: 'absolute',
+        top: '30px',
+        left: '-34px',
+        minWidth: '232px',
+        borderBottomLeftRadius: '4px',
+        borderBottomRightRadius: '4px',
+        overflow: 'hidden',
+        backgroundColor: 'rgb(27, 27, 27)',
+      }}
+    />
+  </div>
 );
