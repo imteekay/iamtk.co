f5f610d9981793969d391bde686e49fdaf2d4d06
TK <leandrotk100@gmail.com>
2021-12-31 19:37:16 -0300

    add url

--- Base/Article/SocialLinks/ShareButtons.tsx
+++ Base/Article/SocialLinks/ShareButtons.tsx
@@ -12,20 +12,39 @@ export const ShareButtons: FC = () => {
   const router = useRouter();
   const path = 'https://iamtk.co' + router.asPath;
   const text = `"${document.title}" by @leandrotk_`;
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${text}&url=${path}`;
+  const linkedinUrl = `https://www.linkedin.com/sharing/share-offsite/?url=${path}`;
+  const facebookUrl = `https://www.facebook.com/sharer.php?u=${path}`;
 
   return (
     <div style={shareButtonsStyle}>
       <a
         className="icon"
-        href={`https://twitter.com/intent/tweet?text=${text}&url=${path}`}
+        href={twitterUrl}
         target="_blank"
         title="twitter share button"
         rel="noreferrer"
       >
         <Icon src="/twitter.svg" />
       </a>
-      <Icon src="/linkedin.svg" />
-      <Icon src="/facebook.svg" />
+      <a
+        className="icon"
+        href={linkedinUrl}
+        target="_blank"
+        title="linkedin share button"
+        rel="noreferrer"
+      >
+        <Icon src="/linkedin.svg" />
+      </a>
+      <a
+        className="icon"
+        href={facebookUrl}
+        target="_blank"
+        title="facebook share button"
+        rel="noreferrer"
+      >
+        <Icon src="/facebook.svg" />
+      </a>
     </div>
   );
 };
