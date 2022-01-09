b9d8a4ee831090d5e4ad12ca74665ae410b4ec8a
TK <leandrotk100@gmail.com>
2022-01-09 14:52:22 -0300

    fix

--- cypress/integration/Home.spec.js
+++ cypress/integration/Home.spec.js
@@ -9,8 +9,11 @@ describe('Home', () => {
     cy.contains("Hi, I'm TK!").should('exist');
 
     postsList.forEach((post) => {
-      cy.contains(post.title).should('exist');
-      cy.contains(post.title).click();
+      cy.get('#writings').within(() => {
+        cy.contains(post.title).should('exist');
+        cy.contains(post.title).click();
+      });
+
       cy.get('[data-testid="home-link"]').click();
     });
   });
