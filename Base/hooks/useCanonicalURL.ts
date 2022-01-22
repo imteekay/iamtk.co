a1f8bc48be6e602f4115ddc419c9117beb72a299
TK <leandrotk100@gmail.com>
2022-01-22 17:50:28 -0300

    -

--- Base/hooks/useCanonicalURL.ts
+++ Base/hooks/useCanonicalURL.ts
@@ -2,6 +2,10 @@ import { useRouter } from 'next/router';
 
 export function useCanonicalURL() {
   const { asPath } = useRouter();
+  console.log(
+    'process.env.NEXT_PUBLIC_DOMAIN_URL',
+    process.env.NEXT_PUBLIC_DOMAIN_URL,
+  );
   return asPath.length === 1
     ? process.env.NEXT_PUBLIC_DOMAIN_URL
     : process.env.NEXT_PUBLIC_DOMAIN_URL + asPath;
