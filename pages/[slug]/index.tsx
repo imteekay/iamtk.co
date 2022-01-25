200343e00846ff9712ccb3a240326a4829dea1ff
TK <leandrotk100@gmail.com>
2022-01-25 17:47:24 -0300

    poc working!

--- pages/[slug]/index.tsx
+++ pages/[slug]/index.tsx
@@ -17,35 +17,33 @@ type PageProps = {
   minutes: number;
 };
 
-const Page: NextPage<PageProps> = ({ postContent, postMetadata, minutes }) => {
-  return (
-    <>
-      <Head
-        title={postMetadata.title}
-        description={postMetadata.description}
-        imageUrl={postMetadata.coverImage.src}
-      />
-      <Layout
-        tags={postMetadata.tags}
-        title={postMetadata.title}
-        date={postMetadata.date}
-        alternativeArticle={postMetadata.alternativeArticle}
-        showSocialLinks
-        minutes={minutes}
-        coverImage={{
-          src: postMetadata.coverImage.src,
-          width: postMetadata.coverImage.width,
-          height: postMetadata.coverImage.height,
-          alt: postMetadata.coverImage.alt,
-          authorHref: postMetadata.coverImage.authorHref,
-          authorName: postMetadata.coverImage.authorName,
-        }}
-      >
-        <div dangerouslySetInnerHTML={{ __html: postContent }} />
-      </Layout>
-    </>
-  );
-};
+const Page: NextPage<PageProps> = ({ postContent, postMetadata, minutes }) => (
+  <>
+    <Head
+      title={postMetadata.title}
+      description={postMetadata.description}
+      imageUrl={postMetadata.coverImage.src}
+    />
+    <Layout
+      tags={postMetadata.tags}
+      title={postMetadata.title}
+      date={postMetadata.date}
+      alternativeArticle={postMetadata.alternativeArticle}
+      showSocialLinks
+      minutes={minutes}
+      coverImage={{
+        src: postMetadata.coverImage.src,
+        width: postMetadata.coverImage.width,
+        height: postMetadata.coverImage.height,
+        alt: postMetadata.coverImage.alt,
+        authorHref: postMetadata.coverImage.authorHref,
+        authorName: postMetadata.coverImage.authorName,
+      }}
+    >
+      <div dangerouslySetInnerHTML={{ __html: postContent }} />
+    </Layout>
+  </>
+);
 
 export async function getStaticPaths() {
   return {
--- pages/links-graph.tsx
+++ pages/links-graph.tsx
@@ -1,7 +1,33 @@
 import type { NextPage } from 'next';
 import ReactFlow from 'react-flow-renderer';
 
-const elements = [
+import Dialog from '@mui/material/Dialog';
+import { Dispatch, SetStateAction, useState } from 'react';
+
+type SimpleDialogPropsType = {
+  open: boolean;
+  onClose: () => void;
+  content: string;
+};
+
+const SimpleDialog = ({ onClose, open, content }: SimpleDialogPropsType) => (
+  <Dialog fullWidth onClose={onClose} open={open}>
+    <div dangerouslySetInnerHTML={{ __html: content }} />
+  </Dialog>
+);
+
+const idToContent = {
+  '1': '<p>Testing id 1</p>',
+  '2': '<p>Testing id 2</p>',
+};
+
+const getElements = ({
+  setContent,
+  setOpen,
+}: {
+  setContent: Dispatch<SetStateAction<string>>;
+  setOpen: Dispatch<SetStateAction<boolean>>;
+}) => [
   {
     id: '1',
     type: 'input',
@@ -10,7 +36,18 @@ const elements = [
   },
   {
     id: '2',
-    data: { label: <div onClick={() => alert('testing')}>Default Node</div> },
+    data: {
+      label: (
+        <div
+          onClick={() => {
+            setContent(idToContent['2']);
+            setOpen(true);
+          }}
+        >
+          Default Node
+        </div>
+      ),
+    },
     position: { x: 100, y: 125 },
   },
   {
@@ -19,10 +56,43 @@ const elements = [
     data: { label: 'Output Node' },
     position: { x: 250, y: 250 },
   },
+  {
+    id: '4',
+    type: 'output',
+    data: { label: 'Output Node' },
+    position: { x: 250, y: 250 },
+  },
+  {
+    id: '5',
+    type: 'output',
+    data: { label: 'Output Node' },
+    position: { x: 250, y: 250 },
+  },
+  {
+    id: '6',
+    type: 'output',
+    data: { label: 'Output Node' },
+    position: { x: 250, y: 250 },
+  },
   { id: 'e1-2', source: '1', target: '2', animated: true },
   { id: 'e2-3', source: '2', target: '3' },
 ];
 
-const Page: NextPage = () => <ReactFlow elements={elements} />;
+const Page: NextPage = () => {
+  const [open, setOpen] = useState(false);
+  const [content, setContent] = useState(`<p>test</p>`);
+  const elements = getElements({ setContent, setOpen });
+
+  return (
+    <>
+      <ReactFlow elements={elements} />
+      <SimpleDialog
+        open={open}
+        onClose={() => setOpen(false)}
+        content={content}
+      />
+    </>
+  );
+};
 
 export default Page;
