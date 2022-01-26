bb685685d4bee34a1ae52402029c39b130c0bb44
TK <leandrotk100@gmail.com>
2022-01-25 23:32:48 -0300

    on close

--- pages/links-graph.tsx
+++ pages/links-graph.tsx
@@ -8,18 +8,16 @@ import { buildGraph } from 'src/lib/buildGraph';
 const Page: NextPage = () => {
   const [open, setOpen] = useState(false);
   const [id, setId] = useState(0);
+
   const graph = buildGraph({ setId, setOpen });
   const { title, content } = idToPost[id];
+
+  const onClose = () => setOpen(false);
 
   return (
     <>
       <ReactFlow elements={graph} />
-      <Dialog
-        open={open}
-        onClose={() => setOpen(false)}
-        title={title}
-        content={content}
-      />
+      <Dialog open={open} onClose={onClose} title={title} content={content} />
     </>
   );
 };
