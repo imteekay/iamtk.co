2f6744c562fa4ce99123baad799fc29876e8ce86
TK <leandrotk100@gmail.com>
2022-01-26 18:43:04 -0300

    remove

--- packages/scripts/src/posts/getPaths.ts
+++ packages/scripts/src/posts/getPaths.ts
@@ -1,27 +1,10 @@
 import path from 'path';
 import fs from 'fs';
 
-type SeriesParams = {
-  series: string;
-  seriesItem: string;
-};
-
-type Params = {
-  params: SeriesParams;
-};
-
-const folderMapper = {
-  bookshelf: 'book',
-  series: 'series',
-  tags: 'tag',
-};
-
 export function getPaths(locale: string = 'en') {
   const postsDir = path.join(__dirname, '../../../..', 'content');
   const postsNames = fs.readdirSync(postsDir);
-  const postsToGeneratePath = postsNames.filter((postName) =>
+  return postsNames.filter((postName) =>
     fs.existsSync(path.join(postsDir, postName, locale)),
   );
-
-  return postsToGeneratePath;
 }
