d959d65ebc19e89350724a7b5e7be01a5a5a19ea
TK <leandrotk100@gmail.com>
2022-01-26 18:33:31 -0300

    Change name

--- scripts/src/index.ts
+++ scripts/src/index.ts
@@ -2,7 +2,7 @@ import { getPostTitle } from './getPostTitle';
 import { getPostContent } from './getPostContent';
 import { getPaths } from './getPaths';
 
-export const posts = getPaths().map((path) => ({
+export const idToPost = getPaths().map((path) => ({
   title: getPostTitle(path),
   content: getPostContent(path),
 }));
