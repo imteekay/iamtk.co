db19d7576a2b5c3ec3e1c34b4197176dfdb7f5d2
TK <leandrotk100@gmail.com>
2022-02-01 10:31:05 -0300

    new positions

--- src/lib/buildGraph.tsx
+++ src/lib/buildGraph.tsx
@@ -3,10 +3,7 @@ import { createGraph } from '@imtk/md-links-graph';
 import { posts } from 'data/markdown-posts';
 
 const urlToPosition = {
-  '/a-mental-model-to-think-in-typescript': {
-    x: 0,
-    y: 0,
-  },
+  '/a-mental-model-to-think-in-typescript': { x: 1500, y: 700 },
   '/actionable-advice-to-start-learning-to-code': {
     x: 0,
     y: 0,
@@ -15,10 +12,7 @@ const urlToPosition = {
     x: 0,
     y: 0,
   },
-  '/basic-recipes-for-react-testing-library': {
-    x: 0,
-    y: 0,
-  },
+  '/basic-recipes-for-react-testing-library': { x: 1400, y: 700 },
   '/building-an-abstraction-for-react-internationalization-messages': {
     x: 1398,
     y: 360,
@@ -27,41 +21,26 @@ const urlToPosition = {
     x: 0,
     y: 0,
   },
-  '/closure-currying-and-cool-abstractions': {
-    x: 0,
-    y: 0,
-  },
-  '/consistent-state-management-in-react-and-redux': {
-    x: 0,
-    y: 0,
-  },
-  '/constant-feedback-driven-development-with-nodemon': {
-    x: 0,
-    y: 0,
-  },
-  '/data-fetching-in-react-with-react-query': {
-    x: 0,
-    y: 0,
-  },
+  '/closure-currying-and-cool-abstractions': { x: 1400, y: 1000 },
+  '/consistent-state-management-in-react-and-redux': { x: 1200, y: 500 },
+  '/constant-feedback-driven-development-with-nodemon': { x: 900, y: 700 },
+  '/data-fetching-in-react-with-react-query': { x: 1200, y: 600 },
   '/designing-my-learning-experience': {
     x: 0,
     y: 0,
   },
-  '/designing-my-lifes-system': {
-    x: 0,
-    y: 0,
-  },
+  '/designing-my-lifes-system': { x: 800, y: 700 },
   '/dx-and-software-maintainability-in-frontend-engineering': {
     x: 1400,
     y: 700,
   },
-  '/fun-with-dates': { x: 0, y: 0 },
+  '/fun-with-dates': { x: 1000, y: 1000 },
   '/functional-programming-principles-in-javascript': {
     x: 0,
     y: 0,
   },
   '/how-i-received-4-salary-raises-in-2-years-of-quintoandar-as-a-software-engineer':
-    { x: 0, y: 0 },
+    { x: 1200, y: 700 },
   '/learning-python-from-zero-to-hero': {
     x: 0,
     y: 0,
@@ -81,19 +60,13 @@ const urlToPosition = {
     y: 0,
   },
   '/publisher-a-tooling-to-automate-the-process-to-publish-my-blog-posts': {
-    x: 0,
-    y: 0,
+    x: 1000,
+    y: 700,
   },
   '/react-hooks-context-api-and-pokemons': { x: 1924, y: 724 },
-  '/react-query-complex-dependent-queries': {
-    x: 0,
-    y: 0,
-  },
+  '/react-query-complex-dependent-queries': { x: 1200, y: 700 },
   '/slow-living': { x: 0, y: 0 },
-  '/tdd-functions-and-react-components': {
-    x: 0,
-    y: 0,
-  },
+  '/tdd-functions-and-react-components': { x: 1200, y: 700 },
   '/thinking-in-data-contracts': { x: 1772, y: 508 },
   '/thoughts-on-my-productivity': {
     x: 0,
@@ -105,10 +78,7 @@ const urlToPosition = {
   },
   '/understanding-the-basics-of-ruby-on-rails-sql-databases-and-how-they-work':
     { x: 0, y: 0 },
-  '/ux-studies-with-react-typescript-and-testing-library': {
-    x: 0,
-    y: 0,
-  },
+  '/ux-studies-with-react-typescript-and-testing-library': { x: 1400, y: 700 },
   '/writing-idiomatic-beautiful-ruby-code': {
     x: 0,
     y: 0,
@@ -125,10 +95,7 @@ const urlToPosition = {
     x: 0,
     y: 0,
   },
-  '/series/frontend-challenges': {
-    x: 0,
-    y: 0,
-  },
+  '/series/frontend-challenges': { x: 1200, y: 500 },
   '/series/mastering-javascript': {
     x: 0,
     y: 0,
@@ -205,24 +172,12 @@ const urlToPosition = {
   },
   '/series/rebuilding-mercaris-search/setup-nextjs-with-typescript-prettier-and-jest':
     { x: 0, y: 0 },
-  '/series/typescript-learnings/interesting-types': {
-    x: 0,
-    y: 0,
-  },
-  '/series/typescript-learnings/object-destructuring': {
-    x: 0,
-    y: 0,
-  },
-  '/series/typescript-learnings/type-system': {
-    x: 0,
-    y: 0,
-  },
-  '/series/typescript-learnings/union-types-with-objects': {
-    x: 0,
-    y: 0,
-  },
+  '/series/typescript-learnings/interesting-types': { x: 1200, y: 900 },
+  '/series/typescript-learnings/object-destructuring': { x: 1200, y: 1000 },
+  '/series/typescript-learnings/type-system': { x: 1200, y: 1100 },
+  '/series/typescript-learnings/union-types-with-objects': { x: 1300, y: 1000 },
   '/series/website-changelog/building-my-legacy-through-accessible-open-and-free-content':
-    { x: 0, y: 0 },
+    { x: 2000, y: 700 },
   '/series/website-changelog/refactoring-setup': {
     x: 0,
     y: 0,
