d5ede7ec460d2c779e8337fd7cd45d92f84b3ae6
imteekay <leandrotk100@gmail.com>
2022-04-03 10:06:02 -0300

    small refactoring

--- pages/microblog/index.tsx
+++ pages/microblog/index.tsx
@@ -1,20 +1,18 @@
 import type { NextPage } from 'next';
 import { Head } from 'Base/components/Head';
 
-const Page: NextPage = () => {
-  return (
-    <>
-      <Head
-        title="TK – Microblog"
-        description="Learning & Improving with TK – Microblog"
-        imageUrl="/logo.jpeg"
-      />
+const Page: NextPage = () => (
+  <>
+    <Head
+      title="TK – Microblog"
+      description="Learning & Improving with TK – Microblog"
+      imageUrl="/logo.jpeg"
+    />
 
-      <main id="main">
-        <div className="content"></div>
-      </main>
-    </>
-  );
-};
+    <main id="main">
+      <div className="content"></div>
+    </main>
+  </>
+);
 
 export default Page;
