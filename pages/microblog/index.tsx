dbb06377bda47100a286c5289af7d78f2483514c
TK <leandrotk100@gmail.com>
2022-04-17 15:22:31 -0300

    share button for microblog

--- pages/microblog/index.tsx
+++ pages/microblog/index.tsx
@@ -5,6 +5,10 @@ import { Post } from 'Base/Microblog/Post';
 import { posts } from 'Base/Microblog/Post/posts';
 import { postWrapperStyle } from 'Base/Microblog/Post/styles';
 
+function toSlug(str: string) {
+  return str.toLocaleLowerCase().split(' ').join('-');
+}
+
 const Page: NextPage = () => (
   <>
     <Head
@@ -18,19 +22,22 @@ const Page: NextPage = () => (
         <HomeLink />
         <h1>Microblog</h1>
 
-        {posts.map((post) => (
-          <div
-            style={postWrapperStyle}
-            key={`${post.title}-${post.description}`}
-          >
-            <Post
-              title={post.title}
-              date={post.date}
-              description={post.description}
-              image={post.image}
-            />
-          </div>
-        ))}
+        {posts.map((post, index) => {
+          const postTitleSlug = toSlug(post.title);
+          const id = `${postTitleSlug}-${index}`;
+
+          return (
+            <div id={id} key={id} style={postWrapperStyle}>
+              <Post
+                title={post.title}
+                date={post.date}
+                description={post.description}
+                image={post.image}
+                id={id}
+              />
+            </div>
+          );
+        })}
       </div>
     </main>
   </>
