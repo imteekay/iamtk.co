d23443ff3c20d70972f988bb7d7f587babfdad64
TK <leandrotk100@gmail.com>
2022-07-19 16:23:40 -0300

    Add pt-BR infra to series

---
+++ pages/pt-BR/series/[series]/[seriesItem]/index.tsx
@@ -0,0 +1,82 @@
+import type { NextPage } from 'next';
+import { GetStaticProps } from 'next';
+import { ParsedUrlQuery } from 'querystring';
+import { getPlaiceholder } from 'plaiceholder';
+import { Head } from 'Base/components/Head';
+import { Layout } from 'Base/Article/Layout';
+import {
+  getSeriesPostContent,
+  getSeriesPostMetadata,
+  PostMetadata,
+  getSeriesPaths,
+} from 'src/lib';
+
+interface Params extends ParsedUrlQuery {
+  series: string;
+  seriesItem: string;
+}
+
+type PageProps = {
+  postContent: string;
+  postMetadata: PostMetadata;
+  minutes: number;
+};
+
+const Page: NextPage<PageProps> = ({ postContent, postMetadata, minutes }) => (
+  <>
+    <Head
+      title={postMetadata.title}
+      description={postMetadata.description}
+      imageUrl={postMetadata.coverImage.src}
+    />
+    <Layout
+      tags={postMetadata.tags}
+      title={postMetadata.title}
+      date={postMetadata.date}
+      alternativeArticle={postMetadata.alternativeArticle}
+      minutes={minutes}
+      showSocialLinks
+      coverImage={postMetadata.coverImage}
+    >
+      <div dangerouslySetInnerHTML={{ __html: postContent }} />
+    </Layout>
+  </>
+);
+
+export async function getStaticPaths() {
+  return {
+    paths: getSeriesPaths(),
+    fallback: false,
+  };
+}
+
+export const getStaticProps: GetStaticProps<PageProps, Params> = async (
+  context,
+) => {
+  const { series, seriesItem } = context.params!;
+  const { postContent, minutes } = getSeriesPostContent(
+    series,
+    seriesItem,
+    'pt-BR',
+  );
+
+  const postMetadata = getSeriesPostMetadata(series, seriesItem, 'pt-BR');
+  const { base64, img } = await getPlaiceholder(postMetadata.coverImage.src);
+
+  return {
+    props: {
+      postContent,
+      postMetadata: {
+        ...postMetadata,
+        coverImage: {
+          ...postMetadata.coverImage,
+          src: img.src,
+          blurDataURL: base64,
+        },
+      },
+      minutes,
+    },
+  };
+};
+
+export default Page;
---
+++ pages/pt-BR/series/[series]/index.tsx
@@ -0,0 +1,83 @@
+import type { NextPage } from 'next';
+import { GetStaticProps } from 'next';
+import { ParsedUrlQuery } from 'querystring';
+import { getPlaiceholder } from 'plaiceholder';
+import { Head } from 'Base/components/Head';
+import { Layout } from 'Base/Article/Layout';
+import {
+  getNestedPaths,
+  getNestedPostContent,
+  getNestedPostMetadata,
+  PostMetadata,
+} from 'src/lib';
+
+interface Params extends ParsedUrlQuery {
+  series: string;
+}
+
+type PageProps = {
+  postContent: string;
+  postMetadata: PostMetadata;
+  minutes: number;
+};
+
+const Page: NextPage<PageProps> = ({ postContent, postMetadata, minutes }) => {
+  return (
+    <>
+      <Head
+        title={postMetadata.title}
+        description={postMetadata.description}
+        imageUrl={postMetadata.coverImage.src}
+      />
+      <Layout
+        tags={postMetadata.tags}
+        title={postMetadata.title}
+        date={postMetadata.date}
+        alternativeArticle={postMetadata.alternativeArticle}
+        minutes={minutes}
+        showSocialLinks
+        coverImage={postMetadata.coverImage}
+      >
+        <div dangerouslySetInnerHTML={{ __html: postContent }} />
+      </Layout>
+    </>
+  );
+};
+
+export async function getStaticPaths() {
+  return {
+    paths: getNestedPaths('series'),
+    fallback: false,
+  };
+}
+
+export const getStaticProps: GetStaticProps<PageProps, Params> = async (
+  context,
+) => {
+  const { series } = context.params!;
+  const { postContent, minutes } = getNestedPostContent(
+    'series',
+    series,
+    'pt-BR',
+  );
+
+  const postMetadata = getNestedPostMetadata('series', series, 'pt-BR');
+  const { base64, img } = await getPlaiceholder(postMetadata.coverImage.src);
+
+  return {
+    props: {
+      postContent,
+      postMetadata: {
+        ...postMetadata,
+        coverImage: {
+          ...postMetadata.coverImage,
+          src: img.src,
+          blurDataURL: base64,
+        },
+      },
+      minutes,
+    },
+  };
+};
+
+export default Page;
