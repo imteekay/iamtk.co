a15ed57f100fc5820d6adbc28de240e0cc9eb649
TK <leandrotk100@gmail.com>
2022-08-28 20:23:07 -0300

    add different perspectives content on the microblog list

--- Base/Microblog/Post/posts.ts
+++ Base/Microblog/Post/posts.ts
@@ -13,6 +13,21 @@ type PostTypes = {
 
 export const posts: PostTypes[] = [
   {
+    title: 'Different Perspectives',
+    slug: 'different-perspectives',
+    date: '2022-08-28 • 20:02',
+    description: `People always tell me: "you're a really good developer", "you're a talented engineer". And instead of nodding and saying "No way, I'm not", I just wanted to say what Mori said to Yatora on Blue Period:
+    <br><br>
+    "It's not talent, I just spend more time thinking about art than most people... To say I have talent is to ignore all my effort."
+    <br><br>
+    I feel the same way as her. I spend so much time on my own craft, improving, learning, doing research, writing, sharing. "But why?", they ask, "Why spending so much time on it?". And I need to quote Masako Saeki, Yatori's teacher:
+    <br><br>
+    "Doesn't it make sense to dedicate yourself to the maximum in the search for what you really like? When a person does what he likes, he is unbeatable."
+    <br><br>
+    I feel exactly like that. Most people don't. I don't judge them for staying comfortable, I know it feels good. But I'm not like that. I'm always in search of the next step, thinking about how I can improve myself and things. It's just how my brain works. It's just a different perspective.
+    `,
+  },
+  {
     title: 'Outcome vs Output',
     slug: 'outcome-vs-output',
     date: '2022-07-20 • 14:46',
