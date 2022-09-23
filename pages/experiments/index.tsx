8c500282a6de7ce851c898c4f88994c2512b2be7
TK <leandrotk100@gmail.com>
2022-09-23 19:02:39 -0300

    refactor

--- pages/experiments/index.tsx
+++ pages/experiments/index.tsx
@@ -3,23 +3,21 @@ import { Head } from 'Base/components/Head';
 import { HomeLink } from 'Base/Article/HomeLink';
 import { Experiments } from 'Home/components/Experiments/Experiments';
 
-const Page: NextPage = () => {
-  return (
-    <>
-      <Head
-        title="TK —— Projects"
-        description="Learning & Improving with TK —— Projects"
-        imageUrl="/logo.jpeg"
-      />
+const Page: NextPage = () => (
+  <>
+    <Head
+      title="TK —— Projects"
+      description="Learning & Improving with TK —— Projects"
+      imageUrl="/logo.jpeg"
+    />
 
-      <main id="main">
-        <div className="content">
-          <HomeLink />
-          <Experiments />
-        </div>
-      </main>
-    </>
-  );
-};
+    <main id="main">
+      <div className="content">
+        <HomeLink />
+        <Experiments />
+      </div>
+    </main>
+  </>
+);
 
 export default Page;
--- pages/web-performance-roadmap/index.tsx
+++ pages/web-performance-roadmap/index.tsx
@@ -2,22 +2,20 @@ import type { NextPage } from 'next';
 import { Head } from 'Base/components/Head';
 import { HomeLink } from 'Base/Article/HomeLink';
 
-const Page: NextPage = () => {
-  return (
-    <>
-      <Head
-        title="TK —— Writings"
-        description="Learning & Improving with TK —— Writings"
-        imageUrl="/logo.jpeg"
-      />
+const Page: NextPage = () => (
+  <>
+    <Head
+      title="TK —— Writings"
+      description="Learning & Improving with TK —— Writings"
+      imageUrl="/logo.jpeg"
+    />
 
-      <main id="main">
-        <div className="content">
-          <HomeLink />
-        </div>
-      </main>
-    </>
-  );
-};
+    <main id="main">
+      <div className="content">
+        <HomeLink />
+      </div>
+    </main>
+  </>
+);
 
 export default Page;
