a68b697ca17dc7f0cf43484ff3c11abad99fbbe1
TK <leandrotk100@gmail.com>
2022-12-04 15:23:43 -0300

    *

--- Base/components/MDX/MDX.tsx
+++ Base/components/MDX/MDX.tsx
@@ -5,6 +5,7 @@ import TweetEmbed from 'react-tweet-embed';
 
 import { PostAndDate } from 'Base/components/PostAndDate';
 import { SideBySideImages } from 'Base/components/SideBySideImages';
+import { SmoothRender } from 'Base/components/SmoothRender';
 
 export type Content = MDXRemoteSerializeResult<
   Record<string, unknown>,
@@ -20,41 +21,11 @@ export const serializeMDX = (content: string) =>
     mdxOptions: { rehypePlugins: [rehypeHighlight] },
   });
 
-import { useState } from 'react';
-import { InView } from 'react-intersection-observer';
-
-const Test = ({ children }) => {
-  const [render, setRender] = useState(false);
-
-  return (
-    <InView as="div" onChange={(inView) => inView && setRender(inView)}>
-      <div
-        style={{
-          opacity: render ? '1' : '0',
-          'transition-timing-function': 'ease',
-          'transition-duration': '0.6s',
-          'transition-delay': '0.124927s',
-        }}
-      >
-        {children}
-      </div>
-    </InView>
-  );
-};
-
-const SmoothlyRender = ({ children }) => (
-  <>
-    {children.map((child, id) => (
-      <Test key={id}>{child}</Test>
-    ))}
-  </>
-);
-
 const components = {
   PostAndDate,
   SideBySideImages,
   TweetEmbed,
-  SmoothlyRender,
+  SmoothRender,
 };
 
 export const MDX = ({ content }: MDXPropTypes) => (
---
+++ Base/components/SmoothRender/index.tsx
@@ -0,0 +1,1 @@
+export { SmoothRender } from './SmoothRender';
