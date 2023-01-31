b4d507986ef02c1980a0faa014266c5be850fef4
TK <leandrotk100@gmail.com>
2023-01-31 08:03:05 -0300

    *

--- Home/components/Bookshelf/bookshelfList.ts
+++ Home/components/Bookshelf/bookshelfList.ts
@@ -1,5 +1,12 @@
 export const bookshelfList = [
   {
+    link: '/bookshelf/what-i-talk-about-when-i-talk-about-running',
+    title: 'What I Talk about When I Talk about Running',
+    description: 'Notes from What I Talk about When I Talk about Running book',
+    target: '',
+    rel: '',
+  },
+  {
     link: '/bookshelf/the-intelligence-trap',
     title: 'The Intelligence Trap',
     description: 'Notes from The Intelligence Trap book',
