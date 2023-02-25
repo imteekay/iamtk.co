ef1502859776b2048b10f2748c3b6b365bf80956
TK <leandrotk100@gmail.com>
2023-02-25 15:05:50 -0300

    *

--- Home/components/Bookshelf/Bookshelf.tsx
+++ Home/components/Bookshelf/Bookshelf.tsx
@@ -2,6 +2,12 @@ import { FC } from 'react';
 import styled from '@emotion/styled';
 import { bookshelfList } from './bookshelfList';
 
+const HackyTableWrapper = styled.div`
+  overflow-x: scroll;
+  margin-left: -1.1rem;
+  margin-right: -1.1rem;
+`;
+
 const Table = styled.table`
   border-radius: 1px;
 `;
@@ -30,39 +36,41 @@ const NoTextDecorationLink = styled.a`
 
 const Columns = () => (
   <colgroup>
-    <col style={{ width: '58%' }} />
-    <col style={{ width: '27%' }} />
-    <col style={{ width: '15%' }} />
+    <col style={{ width: '55%' }} />
+    <col style={{ width: '25%' }} />
+    <col style={{ width: '14%' }} />
   </colgroup>
 );
 
 export const Bookshelf: FC = () => (
   <>
     <h1>Bookshelf</h1>
-    <Columns />
-    <Table>
-      <Tr>
-        <Th>Title</Th>
-        <Th>Author</Th>
-        <Th>Review</Th>
-      </Tr>
-      {bookshelfList.map((book) => (
-        <Tr key={book.link}>
-          <Td>
-            {book.link ? <a href={book.link}>{book.title}</a> : book.title}
-          </Td>
-          <Td>{book.author}</Td>
-          <Td>
-            {book.reviewLink ? (
-              <NoTextDecorationLink href={book.reviewLink} target="_blank">
-                {book.stars}
-              </NoTextDecorationLink>
-            ) : (
-              book.stars
-            )}
-          </Td>
+    <HackyTableWrapper>
+      <Table>
+        <Columns />
+        <Tr>
+          <Th>Title</Th>
+          <Th>Author</Th>
+          <Th>Review</Th>
         </Tr>
-      ))}
-    </Table>
+        {bookshelfList.map((book) => (
+          <Tr key={book.link}>
+            <Td>
+              {book.link ? <a href={book.link}>{book.title}</a> : book.title}
+            </Td>
+            <Td>{book.author}</Td>
+            <Td>
+              {book.reviewLink ? (
+                <NoTextDecorationLink href={book.reviewLink} target="_blank">
+                  {book.stars}
+                </NoTextDecorationLink>
+              ) : (
+                book.stars
+              )}
+            </Td>
+          </Tr>
+        ))}
+      </Table>
+    </HackyTableWrapper>
   </>
 );
