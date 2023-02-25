6daf7e7023e8434922ba2e0ba0cf3b4f1bbe23f1
TK <leandrotk100@gmail.com>
2023-02-25 11:37:11 -0300

    *

--- Home/components/Bookshelf/Bookshelf.tsx
+++ Home/components/Bookshelf/Bookshelf.tsx
@@ -1,19 +1,34 @@
 import { FC } from 'react';
 import { bookshelfList } from './bookshelfList';
 import { List } from 'Base/components/List';
+import styled from '@emotion/styled';
 
 type Header = 'h1' | 'h2';
 type BookshelfPropTypes = { header?: Header };
 
-export const Bookshelf: FC<BookshelfPropTypes> = ({ header }) => (
-  <List
-    sectionId="bookshelf"
-    titleLink="/bookshelf"
-    titleText="bookshelf"
-    list={bookshelfList}
-    openNewTab={false}
-    header={header}
-  />
-);
+const Item = styled.div`
+  border: 1px solid #333;
+  padding: 16px;
+  display: flex;
+  gap: 24px;
+`;
+
+export const Bookshelf: FC<BookshelfPropTypes> = ({ header }) =>
+  // <List
+  //   sectionId="bookshelf"
+  //   titleLink="/bookshelf"
+  //   titleText="bookshelf"
+  //   list={bookshelfList}
+  //   openNewTab={false}
+  //   header={header}
+  // />
+  bookshelfList.map((book) => (
+    <Item>
+      <span>
+        <a href={book.link}>{book.title}</a>
+      </span>
+      <span>{book.description}</span>
+    </Item>
+  ));
 
 export default Bookshelf;
