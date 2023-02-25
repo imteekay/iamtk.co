e833f58030b2a356f160203aca48457de2c27de3
TK <leandrotk100@gmail.com>
2023-02-25 12:22:09 -0300

    *

--- Home/components/Bookshelf/Bookshelf.tsx
+++ Home/components/Bookshelf/Bookshelf.tsx
@@ -1,34 +1,65 @@
 import { FC } from 'react';
-import { bookshelfList } from './bookshelfList';
-import { List } from 'Base/components/List';
 import styled from '@emotion/styled';
+import { bookshelfList } from './bookshelfList';
 
 type Header = 'h1' | 'h2';
 type BookshelfPropTypes = { header?: Header };
 
-const Item = styled.div`
-  border: 1px solid #333;
-  padding: 16px;
-  display: flex;
-  gap: 24px;
+const Table = styled.table`
+  border-radius: 1px;
 `;
 
-export const Bookshelf: FC<BookshelfPropTypes> = ({ header }) =>
-  // <List
-  //   sectionId="bookshelf"
-  //   titleLink="/bookshelf"
-  //   titleText="bookshelf"
-  //   list={bookshelfList}
-  //   openNewTab={false}
-  //   header={header}
-  // />
-  bookshelfList.map((book) => (
-    <Item key={book.link}>
-      <span>
-        <a href={book.link}>{book.title}</a>
-      </span>
-      <span>{book.description}</span>
-    </Item>
-  ));
+const Tr = styled.tr``;
+
+const Th = styled.th`
+  overflow: hidden;
+  padding: 16px;
+  border: 1px solid #333;
+  border-collapse: collapse;
+  border-radius: 1px;
+`;
+
+const Td = styled.td`
+  overflow: hidden;
+  padding: 16px;
+  border: 1px solid #333;
+  border-collapse: collapse;
+  border-radius: 1px;
+`;
+
+const NoTextDecorationLink = styled.a`
+  text-decoration: none;
+`;
+
+export const Bookshelf: FC<BookshelfPropTypes> = () => (
+  <>
+    <h1>Bookshelf</h1>
+    <Table>
+      <colgroup>
+        <col style={{ width: '58%' }} />
+        <col style={{ width: '27%' }} />
+        <col style={{ width: '15%' }} />
+      </colgroup>
+      <Tr>
+        <Th>Title</Th>
+        <Th>Author</Th>
+        <Th>Review</Th>
+      </Tr>
+      {bookshelfList.map((book) => (
+        <Tr key={book.link}>
+          <Td>
+            {book.link ? <a href={book.link}>{book.title}</a> : book.title}
+          </Td>
+          <Td>{book.author}</Td>
+          <Td>
+            <NoTextDecorationLink href={book.reviewLink} target="_blank">
+              {book.stars}
+            </NoTextDecorationLink>
+          </Td>
+        </Tr>
+      ))}
+    </Table>
+  </>
+);
 
 export default Bookshelf;
