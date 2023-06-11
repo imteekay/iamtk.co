e788f5bf7ad53b7b20ce28515faf2821e137eb0d
imteekay <leandrotk100@gmail.com>
2023-06-11 17:00:00 -0300

    *

--- pages/writings.tsx
+++ pages/writings.tsx
@@ -4,23 +4,21 @@ import { HomeLink } from 'Base/Article/HomeLink';
 import { Head } from 'Base/components/Head';
 import { Writings } from 'Home/components/Writings';
 
-const Page: NextPage = () => {
-  return (
-    <>
-      <Head
-        title="TK —— Writings"
-        description="Learning & Improving with TK —— Writings"
-        imageUrl="/logo.jpeg"
-      />
+const Page: NextPage = () => (
+  <>
+    <Head
+      title="TK —— Writings"
+      description="Learning & Improving with TK —— Writings"
+      imageUrl="/logo.jpeg"
+    />
 
-      <main id="main">
-        <div className="content">
-          <HomeLink />
-          <Writings header="h1" />
-        </div>
-      </main>
-    </>
-  );
-};
+    <main id="main">
+      <div className="content">
+        <HomeLink />
+        <Writings header="h1" />
+      </div>
+    </main>
+  </>
+);
 
 export default Page;
