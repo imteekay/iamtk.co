0d369b5d8ba4bda26225f2f7296cc8449b4b5d0f
imteekay <leandrotk100@gmail.com>
2023-06-11 20:13:56 -0300

    *

--- Base/Article/SocialLinks/SocialLinks.tsx
+++ Base/Article/SocialLinks/SocialLinks.tsx
@@ -3,6 +3,7 @@ import { FC } from 'react';
 import { faTwitter, faGithub } from '@fortawesome/free-brands-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
+
 export const SocialLinks: FC = () => (
   <p>
     My{' '}
