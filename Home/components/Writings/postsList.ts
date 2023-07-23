27ab13fee79e65674aebee707ad31cf09f217ba9
imteekay <leandrotk100@gmail.com>
2023-07-23 08:53:57 -0300

    Fix date

--- Home/components/Writings/postsList.ts
+++ Home/components/Writings/postsList.ts
@@ -7,7 +7,7 @@ type Post = {
 
 export const postsList: Post[] = [
   {
-    datetime: '2023-07-21',
+    datetime: '2023-07-23',
     link: '/building-emptystatement-and-semicolon-as-statement-ender-for-the-typescript-compiler',
     title:
       'Building EmptyStatement and Semicolon as a statement ender for the TypeScript compiler',
