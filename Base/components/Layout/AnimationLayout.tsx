086734b6406c6214b59171e6e4be79defc978f5f
imteekay <leandrotk100@gmail.com>
2023-08-03 07:39:23 -0300

    *

--- Base/components/Layout/AnimationLayout.tsx
+++ Base/components/Layout/AnimationLayout.tsx
@@ -1,14 +1,20 @@
 import { FC } from 'react';
 
+import styled from '@emotion/styled';
 import { motion } from 'framer-motion';
 
+const MotionWrapper = styled(motion.div)`
+  // width: 100%;
+  // height: 100%;
+`;
+
 export const AnimationLayout: FC = ({ children }) => (
-  <motion.div
+  <MotionWrapper
     initial={{ opacity: 0 }}
     animate={{ opacity: 1 }}
     exit={{ opacity: 0 }}
     transition={{ delay: 0.3 }}
   >
     {children}
-  </motion.div>
+  </MotionWrapper>
 );
