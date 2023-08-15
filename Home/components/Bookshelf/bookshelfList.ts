4effaaeddb248bb3571f80bb43a87ac6366bde98
imteekay <leandrotk100@gmail.com>
2023-08-15 08:59:42 -0300

    *

--- Home/components/Bookshelf/bookshelfList.ts
+++ Home/components/Bookshelf/bookshelfList.ts
@@ -1,5 +1,12 @@
 export const bookshelfList = [
   {
+    link: '/bookshelf/deep-work',
+    title: 'Deep Work',
+    author: 'Cal Newport',
+    reviewLink: 'https://www.goodreads.com/review/show/4032172628',
+    stars: '⭐️⭐️⭐️⭐️',
+  },
+  {
     link: '/bookshelf/10x-is-easier-than-2x',
     title: '10x Is Easier Than 2x',
     author: 'Benjamin Hardy',
