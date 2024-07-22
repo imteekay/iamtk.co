0f3b5426886c79727a03227470cea4d0c0239f7d
TK <leandrotk100@gmail.com>
2024-07-22 20:56:00 -0300

    *

--- Base/Article/SocialLinks/SocialLinks.tsx
+++ Base/Article/SocialLinks/SocialLinks.tsx
@@ -4,24 +4,28 @@ import { faTwitter, faGithub } from '@fortawesome/free-brands-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export const SocialLinks: FC = () => (
-  <p>
-    <a
-      className="icon"
-      target="_blank"
-      href="https://twitter.com/wordsofteekay"
-      title="twitter"
-      rel="noreferrer"
-    >
-      <FontAwesomeIcon icon={faTwitter} /> Twitter
-    </a>{' '}
-    <a
-      className="icon"
-      target="_blank"
-      href="https://github.com/imteekay"
-      title="github"
-      rel="noreferrer"
-    >
-      <FontAwesomeIcon icon={faGithub} /> Github
-    </a>
-  </p>
+  <>
+    <hr />
+    <p>
+      <a
+        className="icon"
+        target="_blank"
+        href="https://twitter.com/wordsofteekay"
+        title="twitter"
+        rel="noreferrer"
+      >
+        <FontAwesomeIcon icon={faTwitter} /> Twitter
+      </a>
+      {' · '}
+      <a
+        className="icon"
+        target="_blank"
+        href="https://github.com/imteekay"
+        title="github"
+        rel="noreferrer"
+      >
+        <FontAwesomeIcon icon={faGithub} /> Github
+      </a>
+    </p>
+  </>
 );
