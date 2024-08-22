bf0d7559bbb52fa912305ce08b7a0d0847f91587
TK <leandrotk100@gmail.com>
2024-08-22 18:30:59 -0300

    *

--- Base/components/SmoothRender/SmoothRender.tsx
+++ Base/components/SmoothRender/SmoothRender.tsx
@@ -18,6 +18,20 @@ const smoothRenderElementStyle = ({ render }: { render: boolean }) => css`
   opacity: ${render ? '1' : '0'};
 `;
 
+const HEADERS = ['h2', 'h3', 'h4'];
+
+function slugify(text: string) {
+  return text
+    .normalize('NFKD') // The normalize() using NFKD method returns the Unicode Normalization Form of a given string.
+    .toLowerCase() // Convert the string to lowercase letters
+    .trim() // Remove whitespace from both sides of a string
+    .replace(/\s+/g, '-') // Replace spaces with -
+    .replace(/[^\w\-]+/g, '') // Remove all non-word chars
+    .replace(/\_/g, '-') // Replace _ with -
+    .replace(/\-\-+/g, '-') // Replace multiple - with single -
+    .replace(/\-$/g, ''); // Remove trailing -
+}
+
 const SmoothRenderElement = ({ children }: SmoothRenderElementPropTypes) => {
   const [render, setRender] = useState(false);
   const { ref, inView } = useInView({
@@ -36,6 +50,9 @@ const SmoothRenderElement = ({ children }: SmoothRenderElementPropTypes) => {
       <div ref={ref} style={{ minHeight: '0px', visibility: 'hidden' }} />
       {cloneElement(children, {
         className: smoothRenderElementStyle({ render }),
+        id: HEADERS.includes(children.type)
+          ? slugify(children.props.children)
+          : null,
       })}
     </>
   );
