b5f527a9036f91cbcda2461793fb8c86e08997ca
TK <leandro.kinoshita@quintoandar.com.br>
2026-01-29 07:06:44 -0300

    *

--- pages/habit-tracker/index.tsx
+++ pages/habit-tracker/index.tsx
@@ -173,6 +173,7 @@ const data = {
     { date: '2026-01-20', count: 1, level: 4 },
     { date: '2026-01-21', count: 1, level: 4 },
     { date: '2026-01-22', count: 1, level: 4 },
+    { date: '2026-01-28', count: 1, level: 4 },
     { date: '2026-12-31', count: 0, level: 0 },
   ],
   moisturizer: [
