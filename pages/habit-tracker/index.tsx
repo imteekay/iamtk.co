edbed23fb2ca3735f646d56f9761415d28084858
TK <leandro.kinoshita@quintoandar.com.br>
2026-02-24 09:28:01 -0300

    *

--- pages/habit-tracker/index.tsx
+++ pages/habit-tracker/index.tsx
@@ -36,6 +36,7 @@ const data = {
     { date: '2026-02-20', count: 1, level: 4 },
     { date: '2026-02-21', count: 1, level: 4 },
     { date: '2026-02-22', count: 1, level: 4 },
+    { date: '2026-02-24', count: 1, level: 4 },
     { date: '2026-12-31', count: 0, level: 0 },
   ],
   'study-ml': [
@@ -78,6 +79,7 @@ const data = {
     { date: '2026-02-21', count: 1, level: 4 },
     { date: '2026-02-22', count: 1, level: 4 },
     { date: '2026-02-23', count: 1, level: 4 },
+    { date: '2026-02-24', count: 1, level: 4 },
     { date: '2026-12-31', count: 0, level: 0 },
   ],
   'back-workout': [
