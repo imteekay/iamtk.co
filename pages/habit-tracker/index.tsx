8499cd88575e8d5bd5f608ace04cfc164ed24cfb
TK <leandro.kinoshita@quintoandar.com.br>
2026-03-07 00:32:43 -0300

    *

--- pages/habit-tracker/index.tsx
+++ pages/habit-tracker/index.tsx
@@ -181,6 +181,7 @@ const data = {
     { date: '2026-02-25', count: 1, level: 4 },
     { date: '2026-02-28', count: 1, level: 4 },
     { date: '2026-03-04', count: 1, level: 4 },
+    { date: '2026-03-06', count: 1, level: 4 },
     { date: '2026-12-31', count: 0, level: 0 },
   ],
   'practice-guitar': [
@@ -244,6 +245,7 @@ const data = {
     { date: '2026-03-03', count: 1, level: 4 },
     { date: '2026-03-04', count: 1, level: 4 },
     { date: '2026-03-05', count: 1, level: 4 },
+    { date: '2026-03-06', count: 1, level: 4 },
     { date: '2026-12-31', count: 0, level: 0 },
   ],
   'drink-water': [
@@ -299,6 +301,7 @@ const data = {
     { date: '2026-03-03', count: 1, level: 4 },
     { date: '2026-03-04', count: 1, level: 4 },
     { date: '2026-03-05', count: 1, level: 4 },
+    { date: '2026-03-06', count: 1, level: 4 },
     { date: '2026-12-31', count: 0, level: 0 },
   ],
   'intermittent-fasting': [
