84a40ef271ce289bac8c680446291b8fb1be8509
TK <leandro.kinoshita@quintoandar.com.br>
2026-03-08 23:11:49 -0300

    *

--- pages/habit-tracker/index.tsx
+++ pages/habit-tracker/index.tsx
@@ -308,6 +308,7 @@ const data = {
     { date: '2026-03-05', count: 1, level: 4 },
     { date: '2026-03-06', count: 1, level: 4 },
     { date: '2026-03-07', count: 1, level: 4 },
+    { date: '2026-03-08', count: 1, level: 4 },
     { date: '2026-12-31', count: 0, level: 0 },
   ],
   'intermittent-fasting': [
