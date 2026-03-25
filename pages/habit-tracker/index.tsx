f9178486481d9b7162d2f41ec8d75ee8d1618e78
TK <leandro.kinoshita@quintoandar.com.br>
2026-03-25 06:52:28 -0300

    *

--- pages/habit-tracker/index.tsx
+++ pages/habit-tracker/index.tsx
@@ -459,6 +459,7 @@ const data = {
     { date: '2026-03-18', count: 1, level: 4 },
     { date: '2026-03-19', count: 1, level: 4 },
     { date: '2026-03-22', count: 1, level: 4 },
+    { date: '2026-03-24', count: 1, level: 4 },
     { date: '2026-12-31', count: 0, level: 0 },
   ],
 };
