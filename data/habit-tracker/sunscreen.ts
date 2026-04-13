b6bd51b82e37b1697671974992b4c20c3b2c62da
TK <leandro.kinoshita@quintoandar.com.br>
2026-04-13 09:43:32 -0300

    *

--- data/habit-tracker/sunscreen.ts
+++ data/habit-tracker/sunscreen.ts
@@ -67,6 +67,7 @@ const sunscreen = [
   { date: '2026-04-10', count: 1, level: 4 },
   { date: '2026-04-11', count: 1, level: 4 },
   { date: '2026-04-12', count: 1, level: 4 },
+  { date: '2026-04-13', count: 1, level: 4 },
   { date: '2026-12-31', count: 0, level: 0 },
 ];
 
