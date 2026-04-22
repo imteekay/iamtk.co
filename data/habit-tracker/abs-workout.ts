005f5c88df31a35256a45b490a1f69dace3cad8e
TK <leandro.kinoshita@quintoandar.com.br>
2026-04-21 22:40:49 -0300

    *

--- data/habit-tracker/abs-workout.ts
+++ data/habit-tracker/abs-workout.ts
@@ -45,6 +45,7 @@ const absWorkout = [
   { date: '2026-04-13', count: 1, level: 4 },
   { date: '2026-04-14', count: 1, level: 4 },
   { date: '2026-04-19', count: 1, level: 4 },
+  { date: '2026-04-21', count: 1, level: 4 },
   { date: '2026-12-31', count: 0, level: 0 },
 ];
 
