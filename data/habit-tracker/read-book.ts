b466882da02c7e8db4cdd4ec9e205e62f65edc20
TK <leandro.kinoshita@quintoandar.com.br>
2026-05-01 10:49:24 -0300

    *

--- data/habit-tracker/read-book.ts
+++ data/habit-tracker/read-book.ts
@@ -101,6 +101,7 @@ const readBook = [
   { date: '2026-04-27', count: 1, level: 4 },
   { date: '2026-04-28', count: 1, level: 4 },
   { date: '2026-04-29', count: 1, level: 4 },
+  { date: '2026-04-30', count: 1, level: 4 },
   { date: '2026-12-31', count: 0, level: 0 },
 ];
 
