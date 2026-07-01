372093a43e3ab52549ac8966d69c994837df80c0
TK <leandro.kinoshita@quintoandar.com.br>
2026-07-01 07:56:26 -0300

    *

--- data/habit-tracker/practice-guitar.ts
+++ data/habit-tracker/practice-guitar.ts
@@ -8,6 +8,7 @@ const practiceGuitar = [
   { date: '2026-02-02', count: 1, level: 4 },
   { date: '2026-02-03', count: 1, level: 4 },
   { date: '2026-03-16', count: 1, level: 4 },
+  { date: '2026-06-30', count: 1, level: 4 },
   { date: '2026-12-31', count: 0, level: 0 },
 ];
 
