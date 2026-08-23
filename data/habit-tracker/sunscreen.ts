088acf9651d620ca99883e17a4278f38aa17c126
TK <leandro.kinoshita@quintoandar.com.br>
2026-08-23 10:17:57 -0300

    *

--- data/habit-tracker/sunscreen.ts
+++ data/habit-tracker/sunscreen.ts
@@ -179,6 +179,7 @@ const sunscreen = [
   { date: '2026-08-18', count: 1, level: 4 },
   { date: '2026-08-19', count: 1, level: 4 },
   { date: '2026-08-20', count: 1, level: 4 },
+  { date: '2026-08-21', count: 1, level: 4 },
   { date: '2026-12-31', count: 0, level: 0 },
 ];
 
