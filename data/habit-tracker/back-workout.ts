4b6148813e191093c77ed2d793257a9976c33f42
TK <leandro.kinoshita@quintoandar.com.br>
2026-08-24 09:51:51 -0300

    *

--- data/habit-tracker/back-workout.ts
+++ data/habit-tracker/back-workout.ts
@@ -105,6 +105,7 @@ const backWorkout = [
   { date: '2026-08-17', count: 1, level: 4 },
   { date: '2026-08-18', count: 1, level: 4 },
   { date: '2026-08-20', count: 1, level: 4 },
+  { date: '2026-08-23', count: 1, level: 4 },
   { date: '2026-12-31', count: 0, level: 0 },
 ];
 
--- data/habit-tracker/sunscreen.ts
+++ data/habit-tracker/sunscreen.ts
@@ -181,6 +181,7 @@ const sunscreen = [
   { date: '2026-08-20', count: 1, level: 4 },
   { date: '2026-08-21', count: 1, level: 4 },
   { date: '2026-08-22', count: 1, level: 4 },
+  { date: '2026-08-23', count: 1, level: 4 },
   { date: '2026-12-31', count: 0, level: 0 },
 ];
 
