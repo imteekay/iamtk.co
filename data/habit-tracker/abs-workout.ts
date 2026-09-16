5f12797789f741087707c31f89b74545ad8e0daa
TK <leandro.kinoshita@quintoandar.com.br>
2026-09-16 08:19:47 -0300

    *

--- data/habit-tracker/abs-workout.ts
+++ data/habit-tracker/abs-workout.ts
@@ -120,6 +120,7 @@ const absWorkout = [
   { date: '2026-09-09', count: 1, level: 4 },
   { date: '2026-09-10', count: 1, level: 4 },
   { date: '2026-09-13', count: 1, level: 4 },
+  { date: '2026-09-15', count: 1, level: 4 },
   { date: '2026-12-31', count: 0, level: 0 },
 ];
 
--- data/habit-tracker/back-workout.ts
+++ data/habit-tracker/back-workout.ts
@@ -112,6 +112,7 @@ const backWorkout = [
   { date: '2026-09-07', count: 1, level: 4 },
   { date: '2026-09-10', count: 1, level: 4 },
   { date: '2026-09-13', count: 1, level: 4 },
+  { date: '2026-09-15', count: 1, level: 4 },
   { date: '2026-12-31', count: 0, level: 0 },
 ];
 
