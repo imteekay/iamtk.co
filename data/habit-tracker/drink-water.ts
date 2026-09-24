628925a4f577535bb3e6c2555122cc7aef4465cc
TK <leandro.kinoshita@quintoandar.com.br>
2026-09-23 22:03:23 -0300

    *

--- data/habit-tracker/drink-water.ts
+++ data/habit-tracker/drink-water.ts
@@ -234,6 +234,7 @@ const drinkWater = [
   { date: '2026-09-19', count: 1, level: 4 },
   { date: '2026-09-20', count: 1, level: 4 },
   { date: '2026-09-21', count: 1, level: 4 },
+  { date: '2026-09-22', count: 1, level: 4 },
   { date: '2026-12-31', count: 0, level: 0 },
 ];
 
--- data/habit-tracker/sunscreen.ts
+++ data/habit-tracker/sunscreen.ts
@@ -211,6 +211,7 @@ const sunscreen = [
   { date: '2026-09-19', count: 1, level: 4 },
   { date: '2026-09-20', count: 1, level: 4 },
   { date: '2026-09-21', count: 1, level: 4 },
+  { date: '2026-09-22', count: 1, level: 4 },
   { date: '2026-12-31', count: 0, level: 0 },
 ];
 
